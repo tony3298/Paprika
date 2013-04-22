2de609fd705f38eccfcdc351d44a4a14720029ad
Ed <Ed@dhcp-vlan886-63-211-236.wireless.uic.edu>
2013-04-21 19:45:06 -0500

    Added a test .png image to display using a label and setPixmap

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->label->setPixmap(QPixmap("://Images/appbar.close.png"));
 }
 
 MainWindow::~MainWindow()
